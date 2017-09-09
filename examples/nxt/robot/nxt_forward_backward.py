f9f0b475de7064e8b2937ef719c17cea7df35bbd
girafenok <denisbk@gmail.com>
2017-09-10 01:59:10 +0300

    Add comments

--- examples/nxt/robot/nxt_forward_backward.py
+++ examples/nxt/robot/nxt_forward_backward.py
@@ -4,6 +4,8 @@ from nxtrobot import *
 from time import sleep
 
 #max_speed = 1023
+#Conect left motor to port C
+#Conect right motor to port B
 
 robo=NXTRobot("00:16:53:0E:B5:80")#NXT ID <- Settings - NXT Version
 robo.forward(1)
